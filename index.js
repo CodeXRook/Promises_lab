ed40af9679ffd9b2bd9f8317131124934ea73049
CodeXRook <lacekitfly@gmail.com>
2019-02-22 17:39:04 -0500

    alwaysSuccessful promise

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const alwaysSuccessful = () => new Promise(resolve => setTimeout(resolve, 1000))
 const alwaysFailure = () => new Promise((resolve, reject) => setTimeout(reject, 1000))
 const randomNumber = () => new Promise(resolve => setTimeout(() => resolve(Math.floor(Math.random()*10)), 1000))
 const sometimesSuccessful = () => randomNumber().then(k => k % 2 === 0 ? alwaysSuccessful() : alwaysFailure())
+
+let alwaysSuccessful = new Promise(function(resolve, reject){
+    setTimeout(() => resolve(1), 1000);
+})
